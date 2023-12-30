17de4b30858352ebf07db27822d70ae336d384c0
cgarchbold <cgarchbold@gmail.com>
2023-12-30 13:34:19 -0500

    Fixing some issues + Updating README

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # VGKG-Protest-Image-Dataset
 This repository is for converting VGKG url data for image dataset formats for machine learning.
 
+Required Libraries:
+tqdm
+PIL
+
 The Url dataset with corresponding labels can be downloaded at:
 https://blog.gdeltproject.org/vgkg-a-massive-new-5-5-million-global-protest-image-annotations-dataset-from-worldwide-news/
 
-https://blog.gdeltproject.org/vgkg-a-massive-new-5-5-million-global-protest-image-annotations-dataset-from-worldwide-news/#:~:text=compressed%20/%201.2GB%20uncompressed)-,JSON%20Dataset,-.%20(34GB%20compressed%20/%20200GB
+The image urls are contained in the CSV file
 
-This dataset is in JSON format and arrives compressed, full size is 201.2GB
+The labels are in JSON format and arrives compressed, full size is 201.2GB
 
-This project aims to convert these labeled images to an image dataset for protest.
+This project aims to convert these labeled images to an image dataset for protest analysis using computer vision techniques.
--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@ import os
 from urllib import request, error
 from urllib.parse import urlparse
 from PIL import Image
+from tqdm import tqdm
 
 # Path to the CSV file
 csv_file = '/localdisk0/GDELT/2019-vgkg-protest-image-dataset-20191030-labels.csv'
@@ -14,14 +15,14 @@ output_dir = '/localdisk0/GDELT/data/'
 os.makedirs(output_dir, exist_ok=True)
 
 # Function to download an image from a URL
-def download_image(id, img_url, output_dir):
+def download_image(id,index, img_url, output_dir):
     try:
         # Open the URL and read the content
-        with request.urlopen(img_url, timeout=10) as response:
+        with request.urlopen(img_url, timeout=5) as response:
             if response.getcode() == 200:
 
                 parsed_url = urlparse(img_url)
-                filename = os.path.join(output_dir, f"{str(id)}{os.path.splitext(parsed_url.path)[1]}")
+                filename = os.path.join(output_dir, f"{str(index)}_{str(id)}{os.path.splitext(parsed_url.path)[1]}")
                 # Save the image to the output directory
                 with open(filename, 'wb') as f:
                     f.write(response.read())
@@ -50,17 +51,23 @@ def is_valid_image(file_path):
         return True
     except Exception:
         return False
+    
+with open(csv_file, 'r', newline='', encoding='utf-8') as file:
+    total_rows = sum(1 for _ in file)
 
 # Open the CSV file and iterate over its rows
 with open(csv_file, 'r', newline='', encoding='utf-8') as file:
     reader = csv.reader(file, delimiter=',')
     index = 0
-    for row in reader:
+    row_id = 0
+    for row in tqdm(reader, total=total_rows):
         #print(row)
         # Get the image URL from the row
         img_url = row[1]
         #print(img_url)
+        
         # Download the image
-        if download_image(index, img_url, output_dir):
+        if download_image(row_id, index, img_url, output_dir):
             index+=1
+        row_id+=1
 
